61ed83ba81d6878736e8376ee80d6a690b9e0480
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-18 15:57:18 +0200

    index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'open-uri'
+
+
+# url = "https://unsplash.com/photos/htQq9bR5_9A"
+# cocktail = Cocktail.new(title: 'NES')
+# cocktail.remote_photo_url = url
+# cocktail.save
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drink_serialized = URI.parse(url).open.read
+drink = JSON.parse(drink_serialized)
+
+drink['drinks'].each do |d|
+  Ingredient.create(name: d['strIngredient1'])
+end
